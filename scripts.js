2f32f81c7071bdeaef42361af74af0fefe1a5c42
Cody Williamson <codywscw@gmail.com>
2019-04-27 01:20:37 -0500

    reset css, links

---
+++ scripts.js
@@ -0,0 +1,4 @@
+/*
+  minified smooth scroll from w3schools
+*/
+$(document).ready(function(){$("a").on("click",function(o){if(""!==this.hash){o.preventDefault();var t=this.hash;$("html, body").animate({scrollTop:$(t).offset().top},800,function(){window.location.hash=t})}})});
